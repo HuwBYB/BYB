89fa6f9e4e12e111bca8799e6269f60665222191
HuwBYB <huw@chhlogistics.co.uk>
2025-08-08 17:29:48 +0100

    Create ZigQuotes.js

---
+++ src/components/ZigQuotes.js
@@ -0,0 +1,6 @@
+export default [
+  "You don’t have to be great to start, but you have to start to be great.",
+  "People often say motivation doesn’t last. Well, neither does bathing – that’s why we recommend it daily.",
+  "You can get everything in life you want if you will just help enough other people get what they want.",
+  "Lack of direction, not lack of time, is the problem. We all have twenty-four hour days."
+];
