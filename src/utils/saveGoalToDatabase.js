873721f6e406883760c37a7d96b01579743d102a
HuwBYB <huw@chhlogistics.co.uk>
2025-08-08 17:30:50 +0100

    Create saveGoalToDatabase.js

---
+++ src/utils/saveGoalToDatabase.js
@@ -0,0 +1,5 @@
+export async function saveGoalToDatabase(userId, goalData) {
+  // Replace with Firebase/Supabase integration
+  console.log("Saving to DB for:", userId, goalData);
+  // await db.collection("goals").doc(userId).set(goalData);
+}
