b19ba1918f84253e7742a60176d4d871676651eb
HuwBYB <huw@chhlogistics.co.uk>
2025-08-08 17:31:27 +0100

    Create addTasksToTodo.js

---
+++ src/utils/addTasksToTodo.js
@@ -0,0 +1,5 @@
+export async function addTasksToTodo(userId, dailyActions, weeklyActions) {
+  console.log("Adding daily/weekly actions to todos:", dailyActions, weeklyActions);
+  // Loop through daily actions and add to to-do list with repeat rule
+  // Loop through weekly actions similarly
+}
